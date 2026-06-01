575f06188112da51b2d0b2b641866a37ad703319
Ananya Jain <ananyaj1515@gmail.com>
2026-06-01 15:07:16 +0800

    Add logo on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,8 @@ import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import ExploreIcon from '@mui/icons-material/Explore';
 import CardMembershipIcon from '@mui/icons-material/CardMembership';
+import Logo from '../assets/logo.png';
+
 
 const cards = [
     {id: 1, Icon: AutoStoriesIcon, title: "Expert-led courses"},
@@ -17,7 +19,7 @@ function Home() {
     return (
        <div>
             <Navbar />
-            <h1>Welcome to Compass Learn!</h1>
+            <img src={Logo} alt="Compass Learn Logo" style={{ width: '200px', height: '150px' }} />
             <CustomCountdown />
             {cards.map(({id, Icon, title}) => (
                 <Card key={id} Icon={Icon} title={title} />
