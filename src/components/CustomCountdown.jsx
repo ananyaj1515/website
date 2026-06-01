5b890e6cc03250ba300aef5d6d4f5c504fb741c0
Ananya Jain <ananyaj1515@gmail.com>
2026-06-01 12:14:05 +0800

    Implement countdown

--- src/components/CustomCountdown.jsx
+++ src/components/CustomCountdown.jsx
@@ -1,9 +1,39 @@
 import Countdown from 'react-countdown';
+import '../css/CustomCountdown.css';
+
+function CountdownBox({label, value}) {
+    return (
+        <div className="countdown-box">
+            <div className="countdown-value">
+                {String(value).padStart(2, '0')}
+            </div>
+            <div className="countdown-label">
+                {label.toUpperCase()}
+            </div>
+        </div>
+    );
+}
+
+function renderer({days, hours, minutes, seconds, completed}) {
+    if (completed) {
+        return <h1>Launch Time</h1>
+    }
+    return (
+        <div className="countdown-container">
+            <CountdownBox label="Days" value={days} />
+            <CountdownBox label="Hours" value={hours} />
+            <CountdownBox label="Minutes" value={minutes} />
+            <CountdownBox label="Seconds" value={seconds} />
+        </div>
+    );
+}
 
 function CustomCountdown() {
     return (
         <>
-            <Countdown date={new Date('2026-08-01T00:00:00')} />
+            <Countdown date={new Date('2026-08-01T00:00:00')} 
+                renderer={renderer}
+            />
         </>
     )
 }
